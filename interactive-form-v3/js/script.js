f408388b722278af35951fac2323f227e8523042
Russ Tuttle <russelltuttle1@gmail.com>
2021-11-22 16:48:30 -0600

    updated Job Role section
    hid the textbox for "other" when not selected

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,16 @@
 document.getElementById("name").focus();
+
+const otherJobRole = document.getElementById("other-job-role")
+const job = document.getElementById("title")
+otherJobRole.style.display = "none";
+
+//event listener to hide "other job" textbox when not selected
+
+job.addEventListener ("change", ()=> {
+  if (job.value === "other") {
+    otherJobRole.style.display = "block";
+  } else {
+    otherJobRole.style.display = "none";
+  }
+});
+
