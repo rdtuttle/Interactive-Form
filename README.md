5858d0559a76c07219fd390f45e4c0e59e0662c5
Russ Tuttle <russelltuttle1@gmail.com>
2021-11-25 21:04:19 -0600

    updated error messaging

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Interactive-Form
+
+-realtime updated error messaging enabled for "Name" and "credit card number" field.
+
+-conditional error messaging enabled for the "Email" field. Email field will display a different message if the field is left empty or is formatted incorrectly. 
+
+
  
--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,24 +1,32 @@
 const name = document.getElementById("name");
-name.focus();
-
 const otherJobRole = document.getElementById("other-job-role")
 const job = document.getElementById("title")
+const color = document.getElementById("color");
+const design = document.getElementById("design");
+const activities = document.getElementById("activities");
+const totalCost = document.getElementById("activities-cost");
+const pay = document.getElementById("payment");
+const creditCard = document.getElementById("credit-card");
+const paypal = document.getElementById("paypal");
+const bitcoin = document.getElementById("bitcoin");
+const email = document.getElementById("email");
+const ccNum = document.getElementById("cc-num");
+const zip = document.getElementById("zip");
+const cvv = document.getElementById("cvv");
+const form = document.querySelector("form");
+name.focus();
 otherJobRole.style.display = "none";
 
 //event listener to hide "other job" textbox when not selected
-
 job.addEventListener ("change", ()=> {
   if (job.value === "other") {
     otherJobRole.style.display = "block";
-  } else {
+  }else {
     otherJobRole.style.display = "none";
   }
 });
-//set T-Shirt Design to coorespond with matching colors in dropdown menu
-const color = document.getElementById("color");
-const design = document.getElementById("design");
+//event listener to set T-Shirt Design to coorespond with matching colors in dropdown menu
 color.disabled = true;
-
 design.addEventListener("change", () => {
   color.disabled = false;
   for (let i = 0; i < color.children.length; i++) {
@@ -34,11 +42,9 @@ design.addEventListener("change", () => {
     }
   }
 });
-//Adds total price of activities checked
-const activities = document.getElementById("activities");
-const totalCost = document.getElementById("activities-cost");
-let total = 0;
 
+//Adds the total price of the activities checked in the Register for Activities section
+let total = 0;
 activities.addEventListener("change", (e) => {
   const clicked = e.target;
   const dataCost = clicked.getAttribute("data-cost");
@@ -51,17 +57,9 @@ activities.addEventListener("change", (e) => {
   }
   totalCost.innerHTML = `<p>Total: $${total}</p>`
 });
-
-//payment
-
-const pay = document.getElementById("payment");
-const creditCard = document.getElementById("credit-card");
-const paypal = document.getElementById("paypal");
-const bitcoin = document.getElementById("bitcoin");
-
+//displays the paypal/bitcoin information when selected in the dropdown menu
 paypal.style.display = "none";
 bitcoin.style.display = "none";
-
 pay.children[1].setAttribute("selected", "selected");
 pay.addEventListener("change", (e)=> {
   if (e.target.value === "paypal") {
@@ -76,75 +74,60 @@ pay.addEventListener("change", (e)=> {
   }
 });
 
-//form validation
-const email = document.getElementById("email");
-const ccNum = document.getElementById("cc-num");
-const zip = document.getElementById("zip");
-const cvv = document.getElementById("cvv");
-const form = document.querySelector("form");
-
+//function for form validation
+function validation (element, isValid) {
+  if (isValid === false) {
+    event.preventDefault();
+    element.className = "not-valid";
+    element.classList.remove("valid");
+    element.lastElementChild.style.display = "block";
+  } else {
+    element.className = "valid";
+    element.classList.remove("not-valid");
+    element.lastElementChild.style.display = "none"
+  }
+}
 function validName () {
   const nameValue = name.value;
   const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
-  if (nameIsValid === false) {
-    event.preventDefault();
-    name.parentElement.className = "not-valid";
-    name.parentElement.classList.remove("valid");
-    name.parentElement.lastElementChild.style.display = "block";
-  } else {
-    name.parentElement.className = "valid";
-    name.parentElement.classList.remove("not-valid");
-    name.parentElement.lastElementChild.style.display = "none"
-
-  }
+  const nParent = name.parentElement;
+  validation (nParent, nameIsValid);
 }
-
 function validEmail () {
   const emailValue = email.value;
   const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/.test(emailValue);
+  const eParent = email.parentElement;
   if (emailIsValid === false) {
     event.preventDefault();
-    email.parentElement.className = "not-valid";
-    email.parentElement.classList.remove("valid");
-    email.parentElement.lastElementChild.style.display = "block";
-  } else {
-   email.parentElement.className = "valid";
-   email.parentElement.classList.remove("not-valid");
-   email.parentElement.lastElementChild.style.display = "none"
+    if (emailValue === "") {
+      eParent.className = "not-valid";
+      eParent.classList.remove("valid");
+      eParent.lastElementChild.style.display = "block";
+      eParent.lastElementChild.innerHTML = "Email field can't be empty";
+    }else{
+      eParent.className = "not-valid";
+      eParent.classList.remove("valid");
+      eParent.lastElementChild.style.display = "block";
+      eParent.lastElementChild.innerHTML = "Email format is incorrect" }
+  }else{
+   eParent.className = "valid";
+   eParent.classList.remove("not-valid");
+   eParent.lastElementChild.style.display = "none"
 
   }
 }
-
 function validCC () {
   const cardValue = ccNum.value;
   const cardNumberIsValid = /^\d{13,16}$/.test(cardValue);
-  if (cardNumberIsValid === false) {
-    event.preventDefault();
-    ccNum.parentElement.className = "not-valid";
-    ccNum.parentElement.classList.remove("valid");
-    ccNum.parentElement.lastElementChild.style.display = "block";
-  }else  {
-    ccNum.parentElement.className = "valid";
-    ccNum.parentElement.classList.remove("not-valid");
-    ccNum.parentElement.lastElementChild.style.display = "none"
-  }
+  const ccPar = ccNum.parentElement;
+  validation(ccPar, cardNumberIsValid);
 }
-
 function validZip () {
   const zipValue = zip.value;
   const zipIsValid = /[0-9]{5}/.test(zipValue);
-  if (zipIsValid === false) {
-    event.preventDefault();
-    zip.parentElement.className = "not-valid";
-    zip.parentElement.classList.remove("valid");
-    zip.parentElement.lastElementChild.style.display = "block";
-  }else{
-    zip.parentElement.className = "valid";
-    zip.parentElement.classList.remove("not-valid");
-    zip.parentElement.lastElementChild.style.display = "none"
-  }
+  const zipParent = zip.parentElement;
+  validation(zipParent, zipIsValid);
 }
-
 function validForm () {
   if (total = 0) {
     form.parentElement.className = "not-valid";
@@ -152,34 +135,24 @@ function validForm () {
     form.parentElement.lastElementChild.style.display = "block"
   }
 }
-
 function validCVV () {
   const cvvValue = cvv.value;
   const cvvIsValid = /[0-9]{3}/.test(cvvValue);
-  if (cvvIsValid === false) {
+  const cvvParent = cvv.parentElement
+  validation(cvvParent, cvvIsValid);
+}
+function validForm () {
+  if (total === 0) {
     event.preventDefault();
-    cvv.parentElement.className = "not-valid";
-    cvv.parentElement.classList.remove("valid");
-    cvv.parentElement.lastElementChild.style.display = "block"
-  } else  {
-    cvv.parentElement.className = "valid";
-    cvv.parentElement.classList.remove("not-valid");
-    cvv.parentElement.lastElementChild.style.display = "none"
+    activities.classList.add('not-valid');
+    document.getElementById('activities-hint').style.display = 'contents';
+  }
+  if (total !== 0) {
+    activities.classList.add('valid');
+    activities.classList.remove('not-valid');
+    document.getElementById('activities-hint').style.display = 'none';
   }
 }
-
-function validForm () {
-  activities.classList.add('not-valid');
-  document.getElementById('activities-hint').style.display = 'contents';
-if ( total !== 0) {
-  activities.classList.add('valid');
-  activities.classList.remove('not-valid');
-  document.getElementById('activities-hint').style.display = 'none';
-}
-}
-
-
-
 form.addEventListener("submit", () => {
   validName();
   validEmail();
@@ -189,8 +162,7 @@ form.addEventListener("submit", () => {
   validForm();
 });
 
-//accessibility
-
+//adds additional highlighting for accessibility
 const checkbox = document.querySelectorAll("input[type=checkbox]");
 for (let i=0; i < checkbox.length; i++) {
   checkbox[i].addEventListener("focus", () => {
@@ -200,4 +172,32 @@ for (let i=0; i < checkbox.length; i++) {
     checkbox[i].parentElement.classList.remove("focus")
   });
 }
+
+//prevent conflicting registration in the form field for events occuring at the same time
+activities.addEventListener("change", (e) =>{
+  for(let i = 0; i < checkbox.length; i++){
+    if(e.target.checked === true){
+      if(checkbox[i].disabled === false && checkbox[i] !== e.target){
+        if(checkbox[i].parentElement.children[2].textContent === e.target.parentElement.children[2].textContent){
+        checkbox[i].disabled = true;
+      }
+    }
+  }
+    if(e.target.checked === false){
+      if(checkbox[i].disabled === true && checkbox[i] !== e.target){
+        if(checkbox[i].parentElement.children[2].textContent === e.target.parentElement.children[2].textContent){
+        checkbox[i].disabled = false;
+      }
+    }
+    }
+  }
+});
+//keyup listener to update form status in realtime
+name.addEventListener("keyup", (e) => {
+  validName();
+});
+ccNum.addEventListener("keyup", (e) => {
+  validCC();
+});
+
 
